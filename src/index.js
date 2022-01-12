43950a9b17eda4ce9fa4673189a08b004f29af8d
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-01-12 16:11:48 +0200

    dz

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+{
+  /* <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div> */
+}
